0b2105aef50c1e731c1567970b42a837f147dc3f
kinesiologiasanatoriosolano-hash <kinesiologia.sanatorio.solano@gmail.com>
2026-07-03 10:51:42 -0300

    Update settings.gradle.kts

--- mindray-tablet-app/settings.gradle.kts
+++ mindray-tablet-app/settings.gradle.kts
@@ -10,8 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "MindrayReceiver"
 include(":app")
